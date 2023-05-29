93e2929be907231997ecd7773492926c4b737c51
jaydeep <jaydeepk@gmail.com>
2023-05-29 21:31:54 +0530

    Upgraded to specmatic 0.7.4 Fixed tests.

--- test/test_contract_using_api.py
+++ test/test_contract_using_api.py
@@ -17,8 +17,8 @@ class TestContract:
 
 Specmatic.test_asgi_app('app:app',
                         TestContract,
-                        app_contract_file=app_contract_file,
-                        stub_contract_file=stub_contract_file,
+                        app_contracts=[app_contract_file],
+                        stub_contracts=[stub_contract_file],
                         app_host=app_host,
                         app_port=app_port,
                         stub_host=stub_host,
--- test/test_contract_using_decorators.py
+++ test/test_contract_using_decorators.py
@@ -12,9 +12,9 @@ service_contract_file = ROOT_DIR + '/test/spec/product-search-bff-api.yaml'
 stub_contract_file = ROOT_DIR + '/test/spec/api_order_v1.yaml'
 
 
-@specmatic_contract_test(app_host, app_port, ROOT_DIR, service_contract_file)
+@specmatic_contract_test(app_host, app_port, ROOT_DIR, [service_contract_file])
 @start_asgi_app('app:app', app_host, app_port)
-@specmatic_stub(stub_host, stub_port, ROOT_DIR, [expectation_json_file], stub_contract_file)
+@specmatic_stub(stub_host, stub_port, ROOT_DIR, [expectation_json_file], [stub_contract_file])
 class TestContractWithLocalSpecs:
     pass
 
